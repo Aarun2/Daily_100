bd62900563b6a23e32bb372c18072f10c5b1b25a
Aarun2 <43866631+Aarun2@users.noreply.github.com>
2021-08-20 10:53:53 -0500

    Update CPP_10.cpp

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -24,3 +24,24 @@ public:
         return ret;
     }
 };
+
+class Solution { // map holds number to count and each time adding to map or removing from map as needed
+public:
+    int maxOperations(vector<int>& nums, int k) { // map better solution than parsing through back to back for loops O(N^2)
+        
+        unordered_map<int,int>track;    // track number occuerence count
+        
+        int c = 0;  // count sum pairs
+        for(auto &t : nums)
+        {
+            if(track[k-t]>0)    // complement of t exists and so increment count c
+            {
+                track[k-t]--;   // decrement complement item count in Map 
+                c++;
+            }   
+            else track[t]++;    // no complement found and so increase item t count in Map
+        }
+        
+        return c;
+    }
+};
